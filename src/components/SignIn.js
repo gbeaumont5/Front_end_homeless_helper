e9e56aefa7a514272c0f13cae6b51ff2ddec097f
Mushnik <cmorrison@Coreys-Mac.local>
2019-08-20 14:43:54 -0400

    fixed export info for signIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class EditDonor extends Component {}
+class SignIn extends Component {}
 
-export default EditDonor;
+export default SignIn;
