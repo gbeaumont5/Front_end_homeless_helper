0e2129f12806c20b6e9ed4bd67aae7ba6ed39bf5
Gary Beaumont <garyallenbeaumont@gmail.com>
2019-08-21 09:19:33 -0400

    fixed parsing error with Edit member component

--- src/components/EditMember.js
+++ src/components/EditMember.js
@@ -4,14 +4,14 @@ import axios from 'axios';
 class EditMember extends Component {
     constructor(props) {
         super(props)
-        this.state {
-            name:''
-            email: ''
-            bio: ''
-            memberPic: ''
-            medicalNotes: ''
-            skills: ""
-            ableToWork: true
+        this.state = {
+            name:'',
+            email: '',
+            bio: '',
+            memberPic: '',
+            medicalNotes: '',
+            skills: '',
+            ableToWork: true,
             needs: ''
         }
         this.handleChange = this.handleChange.bind(this)
