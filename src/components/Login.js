1ffe81ec8a41fe2c15f3398ff60783220f152e0d
Sammy <asfernandes@sbcglobal.net>
2019-08-22 14:58:57 -0400

    added form to login, not ready though

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class Login extends Component {}
+class Login extends Component {
+    constructor(props) {
+        super(props);
+      }
+    
+      render() {
+        return (
+            <div>
+                <h2>Sign In</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="" placeholder="email" />
+                    <input type="password" placeholder="password" />
+                    <input type="submit">Login</input>
+                </form>
+                </div>
+        )
+
+    }
+}
 
 export default Login;
