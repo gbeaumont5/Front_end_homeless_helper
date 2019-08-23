2ca8b264c53598f287a11535acdd3ca87da147b0
Mushnik <cmorrison@Coreys-Mac.local>
2019-08-23 15:31:24 -0400

    more changes to rectify merge error

--- src/components/UserMainPage.js
+++ src/components/UserMainPage.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import axios from 'axios';
 import toggle from './toggle';
@@ -13,112 +12,96 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 class UserMainPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            members: [],
-            member: {},
-            currentUser: {},
-            userReviews: [],
-            userReview: {},
-            reviewPhoto: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      members: [],
+      member: {},
+      currentUser: {},
+      userReviews: [],
+      userReview: {},
+      reviewPhoto: ''
+    };
+  }
 
-    async getMembers() {
-        const response = await axios(`${baseURL}/members`)
-        const data = response.data
-        this.setState({
-            members: data
-        })
+  async getMembers() {
+    const response = await axios(`${baseURL}/members`);
+    const data = response.data;
+    this.setState({
+      members: data
+    });
+  }
 
-    }
+  async getCurUser() {
+    const response = await axios(`${baseURL}/members/5d60038c51440c439607787b`);
+    const data = response.data;
+    this.setState({
+      currentUser: data[0]
+    });
+    console.log(this.state.currentUser);
+  }
 
-    async getCurUser() {
-        const response = await axios(`${baseURL}/members/5d60038c51440c439607787b`)
-        const data = response.data
-        this.setState({
-            currentUser: data[0]
-        })
-        console.log(this.state.currentUser)
-    }
+  async getUserReviews() {
+    const response = await axios(
+      `${baseURL}/reviews/byUser/5d60038c51440c439607787b`
+    );
+    const data = response.data;
+    this.setState({
+      userReviews: data
+    });
+    console.log('and the user reviews array is:', this.state.userReviews);
+  }
 
-    async getUserReviews () {
-        const response = await axios(`${baseURL}/reviews/byUser/5d60038c51440c439607787b`)
-        const data = response.data
-        this.setState({
-            userReviews: data
-        })
-        console.log('and the user reviews array is:', this.state.userReviews);
-    }
+  componentDidMount() {
+    this.getCurUser();
+    this.getMembers();
+    this.getUserReviews();
+  }
 
-    componentDidMount() {
-        this.getCurUser()
-        this.getMembers()
-        this.getUserReviews()
-    }
-
-    render() {
-        return (
-
-            <div class="content-wrapper">
-
-                <div class="col s12 m7">
-                    <h2 class="header">Welcome Back!</h2>
-                    <div class="card horizontal">
-                        <div class="card-image">
-                            <img src={this.state.currentUser.picture}/>
-                        </div>
-                        <div class="card-stacked">
-                            <div class="card-content">
-                                <h5>{this.state.currentUser.name}</h5>
-                                <h5>{this.state.currentUser.email}</h5>
-                                <button className="btn">Edit</button>
-                            </div>
-                            <div class="card-action">
-                                <a href="#">Delete My Account</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <hr />
-                
-                <h2>My Reviews:</h2>
-
-
-                        
-    
-
-
-
-                // reviews and ratings
-                <div class="flex-container">
-
-                    <div class="reviewbox">
-                        <img src="" width="140px" height="120px" alt="" />
-                        <p>review text goes here</p>
-                        <div>ratings</div>
-                    </div>
-
-                </div>
-
-
-                <hr />
-                <ShowFriends currentUser={this.state.currentUser} />
-
+  render() {
+    return (
+      <div class='content-wrapper'>
+        <div class='col s12 m7'>
+          <h2 class='header'>Welcome Back!</h2>
+          <div class='card horizontal'>
+            <div class='card-image'>
+              <img src={this.state.currentUser.picture} />
             </div>
             <div class='card-stacked'>
               <div class='card-content'>
-                <p>
-                  I am a very simple card. I am good at containing small bits of
-                  information.
-                </p>
+                <h5>{this.state.currentUser.name}</h5>
+                <h5>{this.state.currentUser.email}</h5>
+                <button className='btn' onClick={() => this.props.editMember()}>
+                  Edit
+                </button>
               </div>
               <div class='card-action'>
-                <a href='#'>This is a link</a>
+                <a href='#'>Delete My Account</a>
               </div>
             </div>
+          </div>
+        </div>
+        <hr />
+        <h2>My Reviews:</h2>
+        // reviews and ratings
+        <div class='flex-container'>
+          <div class='reviewbox'>
+            <img src='' width='140px' height='120px' alt='' />
+            <p>review text goes here</p>
+            <div>ratings</div>
+          </div>
+        </div>
+        <hr />
+        <ShowFriends currentUser={this.state.currentUser} />
+        <div class='card-stacked'>
+          <div class='card-content'>
+            <p>
+              I am a very simple card. I am good at containing small bits of
+              information.
+            </p>
+          </div>
+          <div class='card-action'>
+            <a href='#'>This is a link</a>
           </div>
         </div>
         <div class='flex-container'>
