46979ae5f8524a09f358c7b06854a42646f2c9ef
Mushnik <cmorrison@Coreys-Mac.local>
2019-08-23 12:11:58 -0400

    updated friend profile styling

--- src/components/FriendProfile.js
+++ src/components/FriendProfile.js
@@ -5,30 +5,26 @@ import axios from 'axios';
 const baseURL = 'http://localhost:3003';
 
 class FriendProfile extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            friends: [],
-
-        }
-    }
-    render() {
-        return (
-            <div className="row">
-                <div>
-                    <h2>{this.props.friend.name}</h2>
-                    <img alt="pic" src={this.props.friend.picture} />
-                </div>
-                <div>
-
-                </div>
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+  render() {
+    return (
+      <div className='row'>
+        <div className='col s12 m6 center'>
+          <div className='card'>
+            <div className='card-image'>
+              <img alt='pic' src={this.props.friend.picture} />
+              <h2>{this.props.friend.name}</h2>
             </div>
-
-
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-export default FriendProfile
+export default FriendProfile;
