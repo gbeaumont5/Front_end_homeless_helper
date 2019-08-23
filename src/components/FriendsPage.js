98a1de379be0e5bb77357aa5b36d0470c94aa3cf
Mushnik <cmorrison@Coreys-Mac.local>
2019-08-23 11:16:53 -0400

    corrected baseURL on Edit member

--- src/components/FriendsPage.js
+++ src/components/FriendsPage.js
@@ -1,52 +1,45 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 const baseURL = 'http://localhost:3003';
 
 class FriendsPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            friends: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
 
-    componentDidMount() {
-        this.getFriends();
-    }
+  componentDidMount() {
+    this.getFriends();
+  }
 
-    async getFriends() {
-        const response = await axios(`${baseURL}/members`)
-        const data = response.data
-        this.setState({
-            friends: data
-        })
+  async getFriends() {
+    const response = await axios(`${baseURL}/members`);
+    const data = response.data;
+    this.setState({
+      friends: data
+    });
+  }
 
-    }
-
-
-    render() {
-        return (
-            <div>
-                <h3>My Friends</h3>
-                <div class='row'>
-                    {this.state.friends.map(friend => {
-                        return (
-                            <div key={friend._id}>
-                                <h5>{friend.name}</h5>
-                                <img alt="profile picture" src={friend.picture} />
-
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>My Friends</h3>
+        <div class='row'>
+          {this.state.friends.map(friend => {
+            return (
+              <div key={friend._id}>
+                <h5>{friend.name}</h5>
+                <img alt='profile picture' src={friend.picture} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-
-
-
-
-export default FriendsPage
+export default FriendsPage;
